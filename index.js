ba71fd04e78e17b801d0f5c2129bdb417ca87efc
Edwin <edmboche141@gmail.com>
2024-12-13 08:43:22 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.innerHTML = "Edwin is the champion"; 
+document.body.appendChild(newHeader);
